8f2793e84c9c9cc91d91ba0eb0d944df8e01f74e
dzjjss <blankxiv14@gmail.com>
2024-03-04 17:37:40 +0000

    v0.1

---
+++ include/L5.h
@@ -0,0 +1,17 @@
+#ifndef _L5_H
+#define _L5_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class test
+{
+public:
+    test();
+    ~test();
+    void printInfo();
+};
+
+#endif   // _L5_H
---
+++ src/L5/main.cpp
@@ -0,0 +1,24 @@
+#include "L5.h"
+#include <iostream>
+
+using namespace std;
+
+test::test()
+{
+    cout << "test::test()" << endl;
+}
+
+test::~test()
+{
+    cout << "test::~test()" << endl;
+}
+
+void test::printInfo()
+{
+    cout << "test::printInfo()" << endl;
+}
+
+int main()
+{
+    return 0;
+}
